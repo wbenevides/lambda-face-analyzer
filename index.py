c42d994d0e42e3c0ded3f0b4efb91b2d89c077a9
wallacebenevides <wallacebenevides@hotmail.com>
2020-05-02 23:24:57 -0300

    Indexing faces

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 import boto3
 
 s3 = boto3.resource('s3')
+client = boto3.client('rekognition')
+
 
 def list_images():
     images = []
@@ -9,4 +11,20 @@ def list_images():
         images.append(image.key)
     return images
 
+
+def index_images(images):
+    for i in images:
+        response = client.index_faces(
+            CollectionId='faces',
+            Image={
+                'S3Object': {
+                    'Bucket': 'wb-fa-images',
+                    'Name': i
+                }
+            },
+            ExternalImageId=i[:-4],
+            DetectionAttributes=[]
+        )
+
 images = list_images()
+index_images(images)
