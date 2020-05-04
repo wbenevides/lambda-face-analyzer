d373cc36465c2beafeda1d34a719acb3f5bb1940
wallacebenevides <wallacebenevides@hotmail.com>
2020-05-03 21:08:06 -0300

    Deploying data comparison result

--- face_analyze.py
+++ face_analyze.py
@@ -4,6 +4,7 @@ import json
 s3 = boto3.resource('s3')
 client = boto3.client('rekognition')
 
+
 def detect_faces():
     detected_faces = client.index_faces(
         CollectionId='faces',
@@ -18,6 +19,7 @@ def detect_faces():
     )
     return detected_faces
 
+
 def create_detected_id_list(detected_faces):
     id_list = []
     for face in detected_faces['FaceRecords']:
@@ -26,6 +28,37 @@ def create_detected_id_list(detected_faces):
     return id_list
 
 
+def compare_images(id_list):
+    results = []
+    for id in id_list:
+        results.append(client.search_faces(
+            CollectionId='faces',
+            FaceId=id,
+            MaxFaces=10,
+            FaceMatchThreshold=80
+        ))
+    return results
+
+def generate_data_json(compare_result):
+    data_json = []
+    for item in compare_result:
+        face_matches = item.get('FaceMatches')
+        if len(face_matches) >= 1:
+            perfil = dict(name=face_matches[0]['Face']['ExternalImageId'],
+                          faceId=face_matches[0]['Face']['FaceId'],
+                          similarity=round(face_matches[0]['Similarity'], 2)
+                          )
+            data_json.append(perfil)
+    return data_json
+
+
+def deploy_data(data_json):
+    file = s3.Object('wb-fa-site', 'data.json')
+    file.put(Body=json.dumps(data_json))
+
 detected_faces = detect_faces()
 detected_id_list = create_detected_id_list(detected_faces)
-print(detected_id_list)
+compare_result = compare_images(detected_id_list)
+data_json = generate_data_json(compare_result)
+deploy_data(data_json)
+print(json.dumps(data_json, indent=2))
